ff38a932e747aed174b27123354eedbab3e25d0d
lukmir <lukmir2@gmail.com>
2018-04-12 20:40:36 +0200

    rest config class for "/" path

---
+++ src/main/java/com/reactiveenterprise/application/rest/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package com.reactiveenterprise.application.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class ApplicationConfig extends Application {
+}
